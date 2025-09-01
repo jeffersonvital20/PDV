83344b3eaed28f87140a92e7f07075732d20a918
jeffersonvital20 <jeffersonvital20@gmail.com>
2025-08-31 23:30:21 -0300

    feat(jeffersonvital20): Adding readme and adjusting some packages adding .Json collections to help with API testing
    Closes #4

---
+++ PDVAplication/PDVAplication.test/CommandHandler/CreateCustomerCommandHandlerUnitTest.cs
@@ -0,0 +1,11 @@
+namespace PDVAplication.test.CommandHandler
+{
+    public class CreateCustomerCommandHandlerUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
